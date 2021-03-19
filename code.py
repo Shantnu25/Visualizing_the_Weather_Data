99b9556563a8c558603d65041257481174de4d87
Shantnu25 <47518138+Shantnu25@users.noreply.github.com>
2021-03-20 01:56:33 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-# --------------
 # Import the required Libraries
 from matplotlib import pyplot as plt
 import numpy as np
@@ -9,7 +8,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-# Generate a line chart that visualizes the readings in the months
+# Generating a line chart that visualizes the readings in the months
 
 def line_chart(df,period,col):
     """ A line chart that visualizes the readings in the months
@@ -22,19 +21,26 @@ def line_chart(df,period,col):
     col - Feature of the dataframe
     
     """
+    if period == "Month":
+        data = df.groupby(df.index.month).mean()
+    elif period == "Day":
+        data = df.groupby(df.index.day).mean()
+    elif period == "Year":
+        data = df.groupby(df.index.year).mean()
     
-    aggregated_value = df.pivot_table(values=[col], index=period.month,aggfunc='mean')
-
-    plt.figure(figsize=[10,6])
-    plt.xlabel('Months')
-    plt.ylabel('Temp (C)')
+    calendar_months = calendar.month_name[1:]
+    x_series = calendar_months
+    y_series = data[col]
+    
+    plt.plot(x_series, y_series)
     plt.title('Temperature Trend, 2012')
-    my_xticks = ['January','February','March','April','May','June','July','August','September','October','November','December']
-    plt.xticks(aggregated_value.index, my_xticks,rotation ='vertical')
-    plt.plot(aggregated_value.index,aggregated_value['Temp (C)'])
+    plt.xlabel(period)
+    plt.xticks(rotation=90)
+    plt.ylabel(col)
     
     plt.show()
-    
+
+
 
 # Function to perform univariate analysis of categorical columns
 def plot_categorical_columns(df):
@@ -46,16 +52,28 @@ def plot_categorical_columns(df):
     df - Pandas dataframe which has the data.
     
     """
-    plt.figure(figsize=[10,6])
-    plt.xlabel('Weather')
-    plt.ylabel('Frequency')
-    plt.title('Univariate Analysis of Weather')
-    plt.xticks(rotation ='vertical')
-    df_count = df.value_counts()
-
-    plt.bar(df_count.index,df_count.values)
-    plt.show()
+    categorical_columns = df.select_dtypes(include=['object']).columns
     
+    for i in range(0,len(categorical_columns),2):
+            if len(categorical_columns) > i+1:
+                
+                plt.figure(figsize=(10,4))
+                plt.subplot(121)
+                df[categorical_columns[i]].value_counts(normalize=True).plot(kind='bar')
+                plt.title(categorical_columns[i])
+                plt.subplot(122)     
+                df[categorical_columns[i+1]].value_counts(normalize=True).plot(kind='bar')
+                plt.title(categorical_columns[i+1])
+                plt.tight_layout()
+                plt.show()
+
+            else:
+                df[categorical_columns[i]].value_counts(normalize=True).plot(kind='bar')
+                plt.title(categorical_columns[i])
+                plt.show()
+        
+
+
 # Function to plot continous plots
 def plot_cont(df,plt_typ):
     """ Univariate analysis of Numerical columns
@@ -67,14 +85,28 @@ def plot_cont(df,plt_typ):
     plt_type - type of plot through which you want to visualize the data
     
     """
-    plt.figure(figsize=[10,6])
-    if (plt_typ=='distplot'):
-        plt.hist(df,bins=35, color='#0504aa',alpha=0.7)
+    numeric_columns = df.select_dtypes(include=['number']).columns.tolist()
+    df = df[numeric_columns]
+    
+    for i in range(0,len(numeric_columns),2):
+        if len(numeric_columns) > i+1:
+            plt.figure(figsize=(10,4))
+            plt.subplot(121)
+            
+            if plt_typ == "boxplot": 
+                sns.boxplot(df[numeric_columns[i]])
+                plt.subplot(122)            
+                sns.boxplot(df[numeric_columns[i+1]])
+            elif plt_typ == "distplot":
+                sns.distplot(df[numeric_columns[i]])
+                plt.subplot(122)            
+                sns.distplot(df[numeric_columns[i+1]])  
+            else:
+                print("Pass either distplot/boxplot")
+                
+        plt.tight_layout()
+        plt.show()
 
-    else:
-        plt.boxplot(df) 
-
-    plt.show()
 
 # Function to plot grouped values based on the feature
 def group_values(df,col1,agg1,col2):
@@ -91,58 +123,37 @@ def group_values(df,col1,agg1,col2):
     Returns:
     grouping - Dataframe with all columns on which it is grouped on.
     """
-    grouping=df.groupby(col1)[[col2]].agg(agg1)
-    plt.figure(figsize=[10,6])
-    plt.xlabel('Weather')
-    plt.ylabel('Visibility (km)')
-    plt.xticks(rotation ='vertical')
-    plt.bar(grouping.index,grouping['Visibility (km)'])
+    aggregate = {'mean':np.mean,'max':np.max,'min':np.min}
+    grouping = df.groupby(col1).agg(aggregate[agg1])
+    plt.figure(figsize=(10,4))
+    plt.ylabel(col2)
+    grouping[col2].plot(kind="bar")
     plt.show()
-    #print(grouping['Visibility (km)'])
-    return grouping
-
-
 
 
 # Read the Data and pass the parameter as parse_dates=True, index_col='Date/Time'
-weather_df=pd.read_csv(path,parse_dates=True, index_col='Date/Time')
+weather_df = pd.read_csv(path, parse_dates=True, index_col='Date/Time')
+print(weather_df.head(5))
 print(weather_df.shape)
-print('--------------------------------')
 
-print(weather_df.head())
-print('--------------------------------')
-print(weather_df.dtypes)
-print('--------------------------------')
 
 # Lets try to generate a line chart that visualizes the temperature readings in the months.
-# Call the function line_chart() with the appropriate parameters.
-line_chart(weather_df,weather_df.index,'Temp (C)')
-print('--------------------------------')
+# Calling the function line_chart() with the appropriate parameters.
+line_chart(weather_df,"Month","Temp (C)")  
+
 
 # Now let's perform the univariate analysis of categorical features.
-# Call the "function plot_categorical_columns()" with appropriate parameters.
-plot_categorical_columns(weather_df['Weather'])
+# Calling the "function plot_categorical_columns()" with appropriate parameters.
+plot_categorical_columns(weather_df)
 
 
 # Let's plot the Univariate analysis of Numerical columns.
-# Call the function "plot_cont()" with the appropriate parameters to plot distplot
-plot_cont(weather_df['Temp (C)'],'distplot')
+# Calling the function "plot_cont()" with the appropriate parameters to plot distplot
+plot_cont(weather_df,"distplot")
 
 
-# Call the function "plot_cont()" with the appropriate parameters to plot boxplot
-plot_cont(weather_df['Temp (C)'],'boxplot')
+# Calling the function "plot_cont()" with the appropriate parameters to plot boxplot
+plot_cont(weather_df,"boxplot")
 
-# Groupby the data by Weather and plot the graph of the mean visibility during different weathers. Call the function group_values to plot the graph.
-# Feel free to try on diffrent features and aggregated functions like max, min.
-group_values(weather_df,'Weather','mean','Visibility (km)')
-
-
-
-
-
-
-
-
-
-
-
+# Groupby the data by Weather and plot the graph of the mean visibility during different weathers. Calling the function group_values to plot the graph.
+group_values(weather_df,"Weather","mean","Visibility (km)")
